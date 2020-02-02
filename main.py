d75d162a5baa6e02d500e8e7617493d80397c8e1
BHandoshka <b.handoshka@yandex.ru>
2020-02-02 13:30:02 +1000

    Кнопочка синяя))))))

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import pygame
 level = [
     '----------------------------------------------------------------------------------------------------------------------------------',
     '-                                -                                                                                               -',
-    '-    -       -                                              -                 -                                                  -',
+    '-    -      -                                              -                 -                                                   -',
     '-                                     -                                                                      -                   -',
-    '- -             -                                                                             -                                  -',
-    '-  -           -                                                                                                                 -',
-    '-   -----------                                                          -            -                                   -      -',
+    '- -            -                                                                             -                                   -',
+    '-  -          -                                                                                                                  -',
+    '-   ----------                                                          -            -                                   -        -',
     '-                          -                  -                                                  -                               -',
     '-                                                             -                                                                  -',
     '-                                                                                                            -                   -',
@@ -40,6 +40,7 @@ BTN_W, BTN_H = 220, 60
 GOLD = (255, 215, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 
 pygame.init()
 pygame.display.set_caption('первая игра')
@@ -58,13 +59,15 @@ def face(color):
 
 player_rect = player.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2))
 
-text = pygame.font.SysFont('Arial', 22, True, False)
+text = pygame.font.SysFont('Calibri', 22, True, False)
 text_xy = ((WIN_WIDTH - text.size(f'Штрафных очков {round(penalty, 1)}')[0]) // 2, 30)
 
 btn = pygame.Surface((BTN_W, BTN_H))
+btn.fill(BLUE)
 text1 = 'ИГРАТЬ СНОВА?'
 text1_xy = text.size(text1)
 
+color = BLUE
 face(BLUE)
 run = True
 while run:
@@ -83,7 +86,9 @@ while run:
         player_rect.y += PLAYER_SPEED
 
     screen.fill(BG_COLOR)
-    face(BLUE)
+    if color == RED:
+        color = BLUE
+        face(color)
 
     if dx > -WIN_WIDTH * 4:
         dx -= BG_SPEED
@@ -99,17 +104,28 @@ while run:
                 brick = pygame.draw.rect(screen, BRICK_COLOR, [x, y, BRICK_WIDTH, BRICK_HEIGHT])
                 pygame.draw.rect(screen, (255, 128, 0), [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
                 if brick.colliderect(player_rect):
-                    face(RED)
+                    if color == BLUE:
+                        color = RED
+                    face(color)
                     penalty += 0.1
             x += BRICK_WIDTH
         y += BRICK_HEIGHT
         x = dx
 
-    screen.blit(player, player_rect)
-    screen.blit(
-        text.render(f'Штрафных очков {round(penalty, 1)}', True, RED, None), text_xy)
-
-    pygame.display.set_caption(f' FPS: {round(clock.get_fps(), 2)}')
+    if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
+        screen.blit(player, player_rect)
+        screen.blit(
+            text.render(f'Штрафных очков {round(penalty, 1)}', True, RED, None), text_xy)
+    else:
+        screen.blit(btn, ((WIN_WIDTH - BTN_W) // 2, WIN_HEIGHT // 2))
+        btn.blit(
+            text.render(text1, True, WHITE, None),
+            ((BTN_W - text1_xy[0]) // 2, (BTN_H - text1_xy[1]) // 2))
+        screen.blit(
+            text.render(f'Штрафных очков {round(penalty, 1)}', True, RED, None),
+            ((WIN_WIDTH - text.size(f'Штрафных очков {round(penalty, 1)}')[0]) // 2,
+             WIN_HEIGHT // 2 - BTN_H))
+    pygame.display.set_caption
     pygame.display.update()
     clock.tick(FPS)
 
@@ -122,6 +138,5 @@ while run:
 #                     /                   /     print(b, type(b)) /
 #                     \     print(s)      \                       \
 #                     /                   / drive ()              /
-
 # circle = rect, x,y / ellipse = x, y, width, height.
 # RGB = red, green, blue. RGBA = red, green, blue, alpha. => Возможность менять прозрачность.
